a7fa265303f22b3b6e1499920ea5e2d1921742c0
Nikolay.Bilenko <nicholas.bilenko@gmail.com>
2017-10-27 22:09:41 +0300

    conf

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=us-cdbr-iron-east-04.cleardb.net;dbname=heroku_047cb044e59f6a5',
+            'username' => 'bff799ef47fc17',
+            'password' => 'e90ac439',
             'charset' => 'utf8',
         ],
         'mailer' => [

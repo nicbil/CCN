c039e3423856186f1f9cce0cd2ad5bdbbfc475e4
Nikolay.Bilenko <nicholas.bilenko@gmail.com>
2017-10-28 00:15:09 +0300

    post-install-cmd.sh

--- post-install-cmd.sh
+++ post-install-cmd.sh
@@ -2,5 +2,6 @@
 
 php init --env=Production --overwrite=All
 php yii migrate/up --interactive=0
-php yii cache/flush-all
-php yii cache/flush-schema --interactive=0
+#php yii cache/flush-all
+#php yii cache/flush-schema --interactive=0
+composer clear-cache

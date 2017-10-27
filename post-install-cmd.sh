272f4e0b9d9a4d389fecebd46439e568b9e761b2
Nikolay.Bilenko <nicholas.bilenko@gmail.com>
2017-10-27 23:56:51 +0300

    post-install-cmd.sh

--- post-install-cmd.sh
+++ post-install-cmd.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-if [ -n "$DYNO" ]  && [ -n "$ENV" ]; then
-    php init --env=$ENV --overwrite=All
-    php yii migrate/up --interactive=0
-    php yii cache/flush-all
-    php yii cache/flush-schema --interactive=0
-fi
+
+php init --env=2 --overwrite=All
+php yii migrate/up --interactive=0
+php yii cache/flush-all
+php yii cache/flush-schema --interactive=0

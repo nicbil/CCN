10cf8759483e3292aadd988a7c3a3105b8030546
Nikolay.Bilenko <nicholas.bilenko@gmail.com>
2017-10-30 10:25:21 +0300

    post-install-cmd.sh

--- post-install-cmd.sh
+++ post-install-cmd.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 php init --env=Production --overwrite=All
-php yii migrate/up --interactive=0
+#php yii migrate/up --interactive=0
 #php yii cache/flush-all
 #php yii cache/flush-schema --interactive=0
-composer global remove "fxp/composer-asset-plugin"
+#composer global remove "fxp/composer-asset-plugin"
 composer clear-cache
 #rm -rf ~/.composer
 composer self-update
